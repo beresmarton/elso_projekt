e82c3a5ea38820381f12a86f3bea45b9e7747d24
Marton <beresmarton8@gmail.com>
2024-02-04 15:24:29 +0100

    7th commit

--- astronauts.py
+++ astronauts.py
@@ -1,5 +1,6 @@
 # from pprint import pprint
 # import statistics
+# from collections import Counter
 
 
 def beolvaso():
@@ -34,15 +35,33 @@ def elemek_darabszama(szamok):
 
 def sorba_renedzo(darabszamok):
     darabszamok.sort(reverse=True)
-    print(darabszamok)
+    return darabszamok
+
+
+def szazalekolo(ertek, osszes):
+    return round(ertek/osszes*100, 1)
 
 
 def main():
     melyik_mennyi = []
+    sorba_rendezett = sorba_renedzo(elemek_darabszama(megkapja_honapok(beolvaso())))
     for szamlalo in enumerate(elemek_darabszama(megkapja_honapok(beolvaso())), 1):
         melyik_mennyi.append(szamlalo)
-    print(melyik_mennyi)
-    sorba_renedzo(elemek_darabszama(megkapja_honapok(beolvaso())))
+    elso = sorba_rendezett[0]
+    masodik = sorba_rendezett[1]
+    harmadik = sorba_rendezett[2]
+    szam = 0
+    while szam != 12:
+        if (szam, elso) == melyik_mennyi[szam-1]:
+            print(f'Az év {szam}. hónapjában született a legtöbb űrhajós,')
+        if (szam, masodik) == melyik_mennyi[szam-1]:
+            print(f'Az év {szam}. hónapjában született a második legtöbb űrhajós.')
+        if (szam, harmadik) == melyik_mennyi[szam - 1]:
+            print(f'Az év {szam}. hónapjában született a harmadik legtöbb űrhajós.')
+        szam += 1
+    print(f'Az asztronauták {szazalekolo(elso, sum(sorba_rendezett))}%-a augusztusban született. ')
+    print(f'Az asztronauták {szazalekolo(masodik, sum(sorba_rendezett))}%-a májusban született. ')
+    print(f'Az asztronauták {szazalekolo(harmadik, sum(sorba_rendezett))}%-a októberben született. ')
 
 
 main()
