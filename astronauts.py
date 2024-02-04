41763e2734d15cff65c932bb36295e77b15701da
Marton <beresmarton8@gmail.com>
2024-02-04 11:41:06 +0100

    6th commit

--- astronauts.py
+++ astronauts.py
@@ -1,4 +1,7 @@
-from pprint import pprint
+# from pprint import pprint
+# import statistics
+
+
 def beolvaso():
     egy_szemely_adatai = []
     szuletesi_datumok = []
@@ -12,13 +15,34 @@ def beolvaso():
             szuletesi_datumok.append(szuletesi_datum)
         return szuletesi_datumok
 
+
 def megkapja_honapok(lista):
     honapok = []
     for elem in lista:
         elem = elem.split('/')
-        honapok.append(elem[0])
-    pprint(honapok)
+        honapok.append(int(elem[0]))
+    return honapok
 
 
+def elemek_darabszama(szamok):
+    gyakorisag = []
+    for szam in range(1, 13):
+        adott_szam = szamok.count(szam)
+        gyakorisag.append(adott_szam)
+    return gyakorisag
 
-megkapja_honapok(beolvaso())
+
+def sorba_renedzo(darabszamok):
+    darabszamok.sort(reverse=True)
+    print(darabszamok)
+
+
+def main():
+    melyik_mennyi = []
+    for szamlalo in enumerate(elemek_darabszama(megkapja_honapok(beolvaso())), 1):
+        melyik_mennyi.append(szamlalo)
+    print(melyik_mennyi)
+    sorba_renedzo(elemek_darabszama(megkapja_honapok(beolvaso())))
+
+
+main()
